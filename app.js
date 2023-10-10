ba1f5a80744c068e4ea1b9ea0cc42f1e98dd8b9e
Keri96 <kerikanyi@gmail.com>
2023-10-10 12:45:36 +0200

    first

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const app = Vue.createApp({
   //data, function
 
-  template: "<h2>I am the template</h2>",
+  data() {
+    return {
+      url: "http://www.netninja.co.uk",
+      books: [
+        { title: "Elrond uram", author: "Patric Swasey", img: "assets/1.png" },
+        { title: "Dementorcsók", author: "Patric Warrd", img: "assets/2.png" },
+        { title: "Tűzkő", author: "Patric Cloth", img: "assets/3.png" },
+      ],
+      showBooks: true,
+      name: "Keri",
+      EYE_name: "Omnituens",
+      title: "SEMPITERNAL",
+
+      author: "Posty",
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Magic The Gathering";
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
 });
 
 app.mount("#app");
